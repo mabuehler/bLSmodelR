80172e98ccada2698d8f6b638a47ec9bcfa72721
mabuehler <94172633+mabuehler@users.noreply.github.com>
2021-11-12 12:00:23 +0100

    Update add_data.R
    Can you please fill in the option for "all.x = TRUE" or all=TRUE".  all.y = TRUE is probably not really necessary but maybe still nice to have.

--- R/add_data.R
+++ R/add_data.R
@@ -1,7 +1,7 @@
 add_data <- function(x, add, on = intersect(names(x), names(add))){
     temp <- copy(x)
     setDT(temp)
-    out <- merge(temp, add, by = on, all.x = TRUE)
+    out <- merge(temp, add, by = on, all = TRUE)
     # add attributes
     add_attr <- names(attributes(x)) %w/o% c(".internal.selfref", "row.names", "names")
     for(att in add_attr){
